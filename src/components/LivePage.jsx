34cad73cf81c0d472ebe1c26d0beaf7b8b9f312a
MutyalaKalyani <96762970+MutyalaKalyani@users.noreply.github.com>
2024-01-14 21:26:25 +0530

    Update LivePage.jsx

--- src/components/LivePage.jsx
+++ src/components/LivePage.jsx
@@ -1,106 +1,252 @@
-import { Grid ,styled,Box,Button} from '@mui/material';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquare, faThLarge} from '@fortawesome/free-solid-svg-icons'
-import React,{useState} from 'react'
-import img from './img.jpg'
-import ReactPlayer from 'react-player/youtube'
+import { Grid, styled, Box, Button } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCaretDown,
+  faSquare,
+  faThLarge,
+} from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from "react";
+import img from "./img.jpg";
+import "./LivePage.css";
+import ReactPlayer from "react-player/youtube";
 
 export const LivePage = () => {
-  const data = ['Item 1', 'Item 2', 'Item 3', 'Item 4','Item5','Item 6', 'Item 7', 'Item 8', 'Item 9',];
-  function selectFunc()
-  {
-     document.getElementById('one').style.display='block';
-     document.getElementById('four').style.display='none';
-     document.getElementById('nine').style.display='none';
+  const data = [
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item5",
+    "Item 6",
+    "Item 7",
+    "Item 8",
+    "Item 9",
+  ];
+  function selectFunc() {
+    document.getElementById("one").style.display = "block";
+    document.getElementById("four").style.display = "none";
+    document.getElementById("nine").style.display = "none";
   }
-  function selectFunc1(){
-    document.getElementById('one').style.display='none';
-    document.getElementById('four').style.display='block';
-    document.getElementById('nine').style.display='none';
+  function selectFunc1() {
+    document.getElementById("one").style.display = "none";
+    document.getElementById("four").style.display = "block";
+    document.getElementById("nine").style.display = "none";
   }
-  function selectFunc2(){
-    document.getElementById('one').style.display='none';
-    document.getElementById('four').style.display='none';
-    document.getElementById('nine').style.display='block';
+  function selectFunc2() {
+    document.getElementById("one").style.display = "none";
+    document.getElementById("four").style.display = "none";
+    document.getElementById("nine").style.display = "block";
   }
 
+  const [allchecked, setAllChecked] = React.useState([]);
+  function handleChange(e) {
+    if (e.target.checked) {
+      setAllChecked([...allchecked, e.target.value]);
+    } else {
+      setAllChecked(allchecked.filter((item) => item !== e.target.value));
+    }
+  }
+  /* const [displayTrue,setDisplay]=useState(false);
+   function funcOpen()
+   {
+    if(displayTrue)
+    setDisplay(false);
+  else
+    setDisplay(true);
+   }
+   useEffect(()=>{
+     if(displayTrue)
+     document.getElementById('block').style.display='block'
+    else
+    document.getElementById('block').style.display='none'
+   },[displayTrue] );*/
   return (
     <>
-    <div>
-      <div id="one" style={SmallerContainer}>
-      <Grid container style={style} spacing={2} lg={10} md={10} xm={10}>
-      {data.map((item, index) => (
-        <Grid item key={index}  ><ReactPlayer url="https://youtu.be/YRBmZIDqSrc?feature=shared" height="100%" width="100%"/></Grid>
-      ))}
+      <Grid container lg={12}>
+        <Grid item lg={8.8} md={9} xm={8}>
+          <div id="one" style={SmallerContainer}>
+            <Grid container style={style} spacing={2}>
+              {data.map((item, index) => (
+                <Grid item key={index}>
+                  <ReactPlayer
+                    url="https://youtu.be/YRBmZIDqSrc?feature=shared"
+                    height="100%"
+                    width="100%"
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+          <div id="four" style={SmallerContainer1}>
+            <Grid container style={style1} spacing={4} lg={10} md={12}>
+              {data.slice(0, 4).map((item, index) => (
+                <Grid item key={index}>
+                  <ReactPlayer
+                    url="https://youtu.be/YRBmZIDqSrc?feature=shared"
+                    height="100%"
+                    width="100%"
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+          <div id="nine" style={SmallerContainer2}>
+            <Grid container style={style} spacing={1}>
+              <Grid item style={style2}>
+                <ReactPlayer url="https://youtu.be/YRBmZIDqSrc?feature=shared" />
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+        <Grid item lg={3.2} md={3} xm={4}>
+          <div className="color">
+            <Button>
+              <span>
+                <FontAwesomeIcon icon={faCaretDown} size="xl" />
+                Group1
+              </span>
+            </Button>
+            <div id="open">
+              <div>
+                <input value="One" type="checkbox" onChange={handleChange} />
+                <span>CAM1 </span>
+              </div>
+              <div>
+                <input value="Two" type="checkbox" onChange={handleChange} />
+                <span> CAM2 </span>
+              </div>
+              <div>
+                <input value="Three" type="checkbox" onChange={handleChange} />
+                <span> CAM3 </span>
+              </div>
+              <div>
+                <input value="Four" type="checkbox" onChange={handleChange} />
+                <span>CAM4 </span>
+              </div>
+              <div>
+                <input value="Five" type="checkbox" onChange={handleChange} />
+                <span> CAM5 </span>
+              </div>
+              <div>
+                <input value="Five" type="checkbox" onChange={handleChange} />
+                <span> CAM6 </span>
+              </div>
+            </div>
+            <div className="outer_div">
+              <h4 className="title">Transcriptions</h4>
+              <div className="inner_div">
+                <div className="flex">
+                  <h5>CAM1</h5>
+                  <h5>02:50PM</h5>
+                </div>
+                <div className="info">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Itaque saepe dicta fugiat amet minus incidunt quos,{" "}
+                </div>
+              </div>
+              <div className="inner_div">
+                <div className="flex">
+                  <h5>CAM5</h5>
+                  <h5>02:50PM</h5>
+                </div>
+                <div className="info">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Itaque saepe dicta fugiat amet minus incidunt quos,
+                </div>
+              </div>
+            </div>
+            <div className="outer_div">
+              <h4 className="title">Unusual Activity</h4>
+              <div className="inner_div">
+                <div className="flex">
+                  <h5>CAM1</h5>
+                  <h5>02:50PM</h5>
+                </div>
+                <div className="info1">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Itaque saepe dicta fugiat amet minus incidunt quos,{" "}
+                </div>
+              </div>
+              <div className="inner_div">
+                <div className="flex">
+                  <h5>CAM5</h5>
+                  <h5>02:50PM</h5>
+                </div>
+                <div className="info1">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Itaque saepe dicta fugiat amet minus incidunt quos,
+                </div>
+              </div>
+            </div>
+          </div>
+        </Grid>
       </Grid>
-
-    </div>
-    <div id="four" style={SmallerContainer1}  >
-      <Grid container style={style1} spacing={8} >
-      {data.slice(0,4).map((item, index) => (
-        <Grid item key={index} ><ReactPlayer url="https://youtu.be/YRBmZIDqSrc?feature=shared" height="100%" width="100%"/></Grid>
-      ))}
-      </Grid>
-
-    </div>
-      <div id="nine" style={SmallerContainer2}  >
-      <Grid container style={style} spacing={1} lg={10} md={8} xm={8}>
-        <Grid item  style={style2}><ReactPlayer url="https://youtu.be/YRBmZIDqSrc?feature=shared"/></Grid>
-      </Grid>
-
-    </div>
-    </div>
- <Appbar>
- <Button onClick={selectFunc}><img src={img} alt="grid" height={"25px"} width={"25px"} /></Button>
- <Button onClick={selectFunc1}><FontAwesomeIcon icon={faThLarge} size="2xl" style={{color: "#fffafb",}} /></Button>
- <Button onClick={selectFunc2}><FontAwesomeIcon icon={faSquare} size="2xl" style={{color: "#fffafb",}} /></Button>
-</Appbar>
-</>
-  )
-}
-let Appbar=styled(Box)`
-  background:#5a2a75;
-  bottom:1px;
-  position:fixed;
-  height:40px;
-  width:70%;
-  padding:10px 20px;
+      <Appbar>
+        <Button onClick={selectFunc}>
+          <img src={img} alt="grid" height={"25px"} width={"25px"} />
+        </Button>
+        <Button onClick={selectFunc1}>
+          <FontAwesomeIcon
+            icon={faThLarge}
+            size="2xl"
+            style={{ color: "#fffafb" }}
+          />
+        </Button>
+        <Button onClick={selectFunc2}>
+          <FontAwesomeIcon
+            icon={faSquare}
+            size="2xl"
+            style={{ color: "#fffafb" }}
+          />
+        </Button>
+      </Appbar>
+    </>
+  );
+};
+let Appbar = styled(Box)`
+  background: #5a2a75;
+  bottom: 1px;
+  position: fixed;
+  height: 40px;
+  width: 71%;
+  padding: 10px 20px;
 `;
-let style={
-   listStyleType: 'none',
-    display: 'flex', 
-    padding: 0 ,
-}
-let style1={
-  listStyleType: 'none',
-   display: 'flex', 
-   padding: 0 ,
-   margin:'auto',
-}
-let style2={
-  margin:' auto',
-  marginTop:'50px',
-}
+let style = {
+  listStyleType: "none",
+  display: "flex",
+  padding: 0,
+};
+let style1 = {
+  listStyleType: "none",
+  display: "flex",
+  padding: 0,
+  margin: "auto",
+};
+let style2 = {
+  margin: " auto",
+  marginTop: "50px",
+};
 
-  const SmallerContainer={
-    position: "relative",
-    top:'40px',
-    margin:'auto 30px',
+let style4 = {
+  border: "none",
+  background: "transparent",
+  padding: "10px",
+};
+const SmallerContainer = {
+  position: "relative",
+  top: "40px",
+  margin: "auto 30px",
+};
+const SmallerContainer1 = {
+  position: "relative",
+  top: "40px",
+  display: "none",
+  margin: " 30px auto",
+};
 
-  };
-  const SmallerContainer1={
-    position: "relative",
-    top:'40px',
-    margin:'auto 30px',
-    display:'none',
-    width:'65%',
-  };
-
-  const SmallerContainer2={
-    position: "relative",
-    top:'40px',
-    margin:'auto 30px',
-    display:'none',
-    width:'70%',
-  };
-
+const SmallerContainer2 = {
+  position: "relative",
+  top: "40px",
+  display: "none",
+  margin: "auto 30px",
+};
