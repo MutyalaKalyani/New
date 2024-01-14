3c33d781655b3b9e0715ca79a8c1252c10064ceb
MutyalaKalyani <96762970+MutyalaKalyani@users.noreply.github.com>
2024-01-14 18:39:19 +0530

    Update LivePage.jsx

--- src/components/LivePage.jsx
+++ src/components/LivePage.jsx
@@ -1,13 +1,106 @@
-import React from 'react'
+import { Grid ,styled,Box,Button} from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquare, faThLarge} from '@fortawesome/free-solid-svg-icons'
+import React,{useState} from 'react'
+import img from './img.jpg'
+import ReactPlayer from 'react-player/youtube'
 
 export const LivePage = () => {
+  const data = ['Item 1', 'Item 2', 'Item 3', 'Item 4','Item5','Item 6', 'Item 7', 'Item 8', 'Item 9',];
+  function selectFunc()
+  {
+     document.getElementById('one').style.display='block';
+     document.getElementById('four').style.display='none';
+     document.getElementById('nine').style.display='none';
+  }
+  function selectFunc1(){
+    document.getElementById('one').style.display='none';
+    document.getElementById('four').style.display='block';
+    document.getElementById('nine').style.display='none';
+  }
+  function selectFunc2(){
+    document.getElementById('one').style.display='none';
+    document.getElementById('four').style.display='none';
+    document.getElementById('nine').style.display='block';
+  }
+
   return (
-    <div>LivePage
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eos reiciendis aperiam voluptatem iure blanditiis, placeat odit sint dolores minima, nobis vel sit distinctio at, quibusdam ipsam deserunt ducimus maiores.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet deleniti ipsa reprehenderit voluptatem at. Animi fugit ipsa incidunt consequuntur voluptate ex! A porro exercitationem rerum tenetur, eaque ullam repellat autem.
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat saepe itaque modi amet magnam quia. Tempora quis est, vero quaerat itaque, saepe similique dolorem reiciendis adipisci dolore reprehenderit quos laboriosam?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo distinctio ipsa similique inventore, modi quo atque libero dolorum ipsum in velit ut necessitatibus mollitia voluptatem eligendi quos vero ab nostrum.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam doloribus consequatur sint quos accusantium nihil eaque dicta omnis ratione iure, vero culpa odit incidunt ipsa accusamus eius ipsum cum consectetur.
+    <>
+    <div>
+      <div id="one" style={SmallerContainer}>
+      <Grid container style={style} spacing={2} lg={10} md={10} xm={10}>
+      {data.map((item, index) => (
+        <Grid item key={index}  ><ReactPlayer url="https://youtu.be/YRBmZIDqSrc?feature=shared" height="100%" width="100%"/></Grid>
+      ))}
+      </Grid>
+
     </div>
+    <div id="four" style={SmallerContainer1}  >
+      <Grid container style={style1} spacing={8} >
+      {data.slice(0,4).map((item, index) => (
+        <Grid item key={index} ><ReactPlayer url="https://youtu.be/YRBmZIDqSrc?feature=shared" height="100%" width="100%"/></Grid>
+      ))}
+      </Grid>
+
+    </div>
+      <div id="nine" style={SmallerContainer2}  >
+      <Grid container style={style} spacing={1} lg={10} md={10} xm={10}>
+        <Grid item  style={style2}><ReactPlayer url="https://youtu.be/YRBmZIDqSrc?feature=shared"/></Grid>
+      </Grid>
+
+    </div>
+    </div>
+ <Appbar>
+ <Button onClick={selectFunc}><FontAwesomeIcon icon={faSquare} size="2xl" style={{color: "#fffafb",}} /></Button>
+ <Button onClick={selectFunc1}><FontAwesomeIcon icon={faThLarge} size="2xl" style={{color: "#fffafb",}} /></Button>
+ <Button onClick={selectFunc2}><img src={img} alt="grid" height={"25px"} width={"25px"} /></Button>
+</Appbar>
+</>
   )
 }
+let Appbar=styled(Box)`
+  background:#5a2a75;
+  bottom:1px;
+  position:fixed;
+  height:40px;
+  width:70%;
+  padding:10px 20px;
+`;
+let style={
+   listStyleType: 'none',
+    display: 'flex', 
+    padding: 0 ,
+}
+let style1={
+  listStyleType: 'none',
+   display: 'flex', 
+   padding: 0 ,
+   margin:'auto',
+}
+let style2={
+  margin:' auto',
+  marginTop:'50px',
+}
+
+  const SmallerContainer={
+    position: "relative",
+    top:'40px',
+    margin:'auto 30px',
+
+  };
+  const SmallerContainer1={
+    position: "relative",
+    top:'40px',
+    margin:'auto 30px',
+    display:'none',
+    width:'65%',
+  };
+
+  const SmallerContainer2={
+    position: "relative",
+    top:'40px',
+    margin:'auto 30px',
+    display:'none',
+    width:'70%',
+  };
+
